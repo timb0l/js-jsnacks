d166f0cfe9fd94f854fd16c45eb08e1ad8988171
Jam Timbol <jam.timbol00@gmail.com>
2023-07-06 16:15:21 +0200

    Template

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Milestone 1:
+//Crea una funzione chiamata sommaNumeri che prenda un array di N numeri
+//e calcoli la somma di tutti questi numeri contenuti nell'array, restituendolo come risultato.
+//Utilizzare la funzione sommaNumeri per stampare il risultato.
+//L'array di N numeri da passare alla funzione lo create chiedendo all'utente quanti numeri vuole inserire (cioè N),
+//e poi facendogli inserire questi N numeri all'utente stesso tramite apposito prompt().
+
